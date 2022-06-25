ef051079d79122384fc46b9e24c4516d6745f1fe
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-25 21:52:01 +0300

    add useRef to the form in NewTodo

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,12 +1,24 @@
+import { useRef } from 'react';
+
 const NewTodo = () => {
+  const textInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    // const enteredText = textInputRef.current?.value;
+    // submitHandler is definitely called after ref is assigned in returned jsx
+    const enteredText = textInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">To-do text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={textInputRef} />
       <button>Add To-do</button>
     </form>
   );
