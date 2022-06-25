7d7ed3f13da55edbdcdcdf11e315efe2cfbd2f12
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-25 20:28:00 +0300

    add Todos, use react functional component FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Todos from './components/Todos';
 
 function App() {
   return (
     <div>
-      
+      <Todos items={['Learn React', 'Learn TypeScript']} />
     </div>
   );
 }
